7a51f5d6c9566999f35e399852ae882550f98531
Arron Linton <lintonarron@gmail.com>
2017-12-21 22:11:39 -0500

    siginIn function works

--- src/app/user/auth.service.ts
+++ src/app/user/auth.service.ts
@@ -5,11 +5,17 @@ import { Subject } from 'rxjs/Subject';
 import { Observable } from 'rxjs/Observable';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 // import cognitouserpool from amazon cognito identity js package
-import { CognitoUserPool, CognitoUserAttribute, CognitoUser, AuthenticationDetails, CognitoUserSession } from 'amazon-cognito-identity-js'
+import { 
+  CognitoUserPool, 
+  CognitoUserAttribute, 
+  CognitoUser, 
+  AuthenticationDetails, 
+  CognitoUserSession } from 'amazon-cognito-identity-js'
 import { User } from './user.model';
 
 // user pool id and client id can both be found in the amazon cognito console inside your created user pool
 const poolData = {
+  // inside objects cognito requries captial letters for the key(s)
   UserPoolId : 'us-east-1_tewEF6OM0', 
   ClientId : 'ljpsbipujidvolifnkpuo54it' 
 };
@@ -26,10 +32,7 @@ export class AuthService {
   authStatusChanged = new Subject<boolean>();
   registeredUser: CognitoUser;
 
-  constructor(private router: Router) {
-
-  }
-
+  constructor(private router: Router) {}
 
   // signUp method
   signUp(username: string, email: string, password: string): void {
@@ -42,13 +45,13 @@ export class AuthService {
     };
     // password and user and default attributes so we do not explicitly need to configure them
     // we do need to configure the email
-    // 
+    //
     const attrList: CognitoUserAttribute[] = [];
     // create new email attribute object
     // holds 2 keys --> name and value
     const emailAttribute = {
       // name --> name of attribute we selected in cognito
-      Name: 'email',
+      Name: "email",
       // value --> what we want to pass as a value (i.e. user email)
       Value: user.email
     };
@@ -58,73 +61,81 @@ export class AuthService {
     // use pool object and call the signUp method
     // null --> used for additional validation data which we could use to validate the information on the server
     // callback -- > getes executed once amazon cognito is done with the http request on the back end cognito servers
-    userPool.signUp(user.username, user.password, attrList, null, function(err, result){
-      if(err){
+    userPool.signUp(user.username, user.password, attrList, null, function(
+      err,
+      result
+    ) {
+      if (err) {
         // what does .next do ?
         // this.authDidFail.next(true);
         // this.authIsLoading.next(false);
-        console.log('authentication err', err)
+        console.log("authentication err", err);
         // return;
-      }else{
-        console.log("authenticating user")
-      // custom functions
-      // this.authDidFail.next(false)
-      // this.authIsLoading.next(false);
-      // this.registeredUser = result.user;
+      } else {
+        console.log("authenticating user");
+        // custom functions
+        // this.authDidFail.next(false)
+        // this.authIsLoading.next(false);
+        // this.registeredUser = result.user;
 
-      // console.log('user name is' + this.registeredUser.getUsername())
+        // console.log('user name is' + this.registeredUser.getUsername())
       }
     });
     return;
   }
 
+  // Confirming a registered, unauthenticated user using a confirmation code received via SMS or email
   confirmUser(username: string, code: string) {
     this.authIsLoading.next(true);
+
     const userData = {
       Username: username,
       Pool: userPool
     };
+
     const cognitoUser = new CognitoUser(userData);
     cognitoUser.confirmRegistration(code, true, (err, result) => {
-      if(err){
-        console.log('confirm failed', err)
-      }else{
-        console.log('confirmation successful')
+      if (err) {
+        console.log("confirm failed", err);
+      } else {
+        console.log("confirmation successful");
         // redirect to the / route --> signIn page
-        this.router.navigate(['/']);
+        this.router.navigate(["/"]);
       }
-    })
+    });
   }
 
-
+  // Authenticating a user and establishing a user session with the Amazon Cognito Identity service.
   signIn(username: string, password: string): void {
     this.authIsLoading.next(true);
+    // username and password to be authenticated
     const authData = {
       Username: username,
       Password: password
     };
-
+// configure new authentication details oject and pass in user authData
     const authDetails = new AuthenticationDetails(authData);
-
+// userData object associated with respective userPool
     const userData = {
       Username: username,
       Pool: userPool
     };
+    // create new cognitoUser with the userData in order to initialize that user
     const cognitoUser = new CognitoUser(userData);
 
     // need to bind "this" to a constant which can be used inside the function below
     const that = this;
+
     cognitoUser.authenticateUser(authDetails, {
-      onSuccess (result: CognitoUserSession){
-        console.log(result)
-        console.log('access token', result.getAccessToken().getJwtToken());
+      onSuccess(result: CognitoUserSession) {
+        console.log(result);
+        console.log("access token", result.getAccessToken().getJwtToken());
 
         // that.authStatusChange.next(true);
-        console.log('auth sucessful !')
+        console.log("auth sucessful !");
       },
-      onFailure(err){
-        console.log('failed auth', err)
-
+      onFailure(err) {
+        console.log("failed auth", err);
       }
     });
 
@@ -133,16 +144,25 @@ export class AuthService {
   }
 
   getAuthenticatedUser() {
+    return userPool.getCurrentUser();
+    // cognitoUser.getUserAttributes(function(err, result) {
+    //     if (err) {
+    //         alert(err);
+    //         return;
+    //     }
+    //     for (i = 0; i < result.length; i++) {
+    //         console.log('attribute ' + result[i].getName() + ' has value ' + result[i].getValue());
+    //     }
+    // });
   }
 
   logout() {
     this.authStatusChanged.next(false);
   }
 
-
   isAuthenticated(): Observable<boolean> {
     const user = this.getAuthenticatedUser();
-    const obs = Observable.create((observer) => {
+    const obs = Observable.create(observer => {
       if (!user) {
         observer.next(false);
       } else {
@@ -153,8 +173,6 @@ export class AuthService {
     return obs;
   }
   initAuth() {
-    this.isAuthenticated().subscribe(
-      (auth) => this.authStatusChanged.next(auth)
-    );
+    this.isAuthenticated().subscribe(auth => this.authStatusChanged.next(auth));
   }
 }
